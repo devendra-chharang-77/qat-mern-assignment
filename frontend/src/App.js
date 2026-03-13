9f5574d1fc67e0a9d87b0a872c5a2a48c120a810
DevendraChharang <devendrachharang366@gmail.com>
2026-03-13 11:37:49 +0530

    Fixed root route and white screen issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        {/* Ye line missing hai! Home page ko Login par bhej do */}
+        <Route path="/" element={<Navigate to="/login" />} /> 
+        
         <Route path="/login" element={<Login />} />
-        {}
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
